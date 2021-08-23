df0501516c2f54b8bd905b0d3f5d0a26fa5547c0
Tasnuva Rahman <tasnuva@codemen.net>
2021-08-23 11:57:18 +0600

    tasnuva:added tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,11 @@ module.exports = {
   purge: { content: ['./public/**/*.html', './src/**/*.vue'] },
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      spacing: {
+        "row": "calc(100% + 2rem)",
+      },
+    },
   },
   variants: {
     extend: {},
