95a6be699db2ef361f1b798488f76a9045b579b9
jamil-codemen <jamil@codemen.net>
2021-08-23 12:55:06 +0600

    Codemen Official
    	1:Team Page and route created

---
+++ src/views/OurTeam.vue
@@ -0,0 +1,206 @@
+<template>
+   <div class="bradclumb relative py-32">
+        <div class="bg-center bg-gray-800"></div>
+        <img src="" alt="image1" class="object-cover w-full h-full absolute top-0 left-0">
+        <div class="container relative z-10 mx-auto md:px-6 px-4 flex flex-wrap items-center justify-center">
+            <div class="slide-content flex sm:text-left justify-center 2xl:w-1/3 xl:w-2/3 sm:w-1/2 w-ful sm:mt-0 mt-32">
+                <div class="">
+                    <nav class="">
+                        <ol class="breadcrumb flex flex-wrap justify-center font-medium text-xl">
+                            <li class="breadcrumb-item text-white"><a href="index.html">Home</a></li>
+                            <span class="px-2 text-white">/</span>
+                            <li class="breadcrumb-item text-green-500  active:text-green-500 " aria-current="page">Our
+                                Team</li>
+                        </ol>
+                    </nav>
+                    <h1 class="md:leading-tight title md:text-7xl text-4xl font-black text-white mt-1.5">
+                        Our Team
+                    </h1>
+                </div>
+            </div>
+        </div>
+    </div>
+<section id="our_team" class="md:px-24 px-0 py-24">
+        <div class="container max-w-screen-xl mx-auto md:px-6 px-4">
+            <div class="">
+                <div class="flex flex-wrap justify-center">
+                    <div class="md:w-1/4 sm:w-1/2 w-full px-4">
+                        <div class="group md:mt-0 mt-10 ">
+                            <div class="bg-green-50 w-full h-full overflow-hidden">
+                                <img class="bg-bottom transition duration-500 ease-in-out transform hover:-translate-y-1 hover:scale-110 mx-auto"
+                                    src=""  alt="">
+                            </div>
+                            <div class="text-center pt-6">
+                                <p class="font-semibold text-3xl">FORHAD HOSSEN</p>
+                                <p class="">Web Developer</p>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="md:w-1/4 sm:w-1/2 w-full px-4">
+                        <div class="group md:mt-0 mt-10 ">
+                            <div class="bg-green-50 w-full h-full overflow-hidden">
+                                <img class="bg-bottom transition duration-500 ease-in-out transform hover:-translate-y-1 hover:scale-110 mx-auto"
+                                    src=""  alt="">
+                            </div>
+                            <div class="text-center pt-6">
+                                <p class="font-semibold text-3xl">FORHAD HOSSEN</p>
+                                <p class="">Web Developer</p>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="md:w-1/4 sm:w-1/2 w-full px-4">
+                        <div class="group md:mt-0 mt-10 ">
+                            <div class="bg-green-50 w-full h-full overflow-hidden">
+                                <img class="bg-bottom transition duration-500 ease-in-out transform hover:-translate-y-1 hover:scale-110 mx-a"
+                                    src=""  alt="">
+                            </div>
+                            <div class="text-center pt-6">
+                                <p class="font-semibold text-3xl">FORHAD HOSSEN</p>
+                                <p class="">Web Developer</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="flex flex-wrap justify-center">
+                    <div class="md:w-1/4 sm:w-1/2 w-full px-4">
+                        <div class="group mt-16 ">
+                            <div class="bg-green-50 w-full h-full overflow-hidden">
+                                <img class="bg-bottom transition duration-500 ease-in-out transform hover:-translate-y-1 hover:scale-110 mx-auto"
+                                    src=""  alt="">
+                            </div>
+                            <div class="text-center pt-6">
+                                <p class="font-semibold text-3xl">FORHAD HOSSEN</p>
+                                <p class="">Web Developer</p>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="md:w-1/4 sm:w-1/2 w-full px-4">
+                        <div class="group mt-16 ">
+                            <div class="bg-green-50 w-full h-full overflow-hidden">
+                                <img class="bg-bottom transition duration-500 ease-in-out transform hover:-translate-y-1 hover:scale-110 mx-auto"
+                                    src=""  alt="">
+                            </div>
+                            <div class="text-center pt-6">
+                                <p class="font-semibold text-3xl">FORHAD HOSSEN</p>
+                                <p class="">Web Developer</p>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="md:w-1/4 sm:w-1/2 w-full px-4">
+                        <div class="group mt-16 ">
+                            <div class="bg-green-50 w-full h-full overflow-hidden">
+                                <img class="bg-bottom transition duration-500 ease-in-out transform hover:-translate-y-1 hover:scale-110 mx-auto"
+                                    src=""  alt="">
+                            </div>
+                            <div class="text-center pt-6">
+                                <p class="font-semibold text-3xl">FORHAD HOSSEN</p>
+                                <p class="">Web Developer</p>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="md:w-1/4 sm:w-1/2 w-full px-4">
+                        <div class="group mt-16 ">
+                            <div class="bg-green-50 w-full h-full overflow-hidden">
+                                <img class="bg-bottom transition duration-500 ease-in-out transform hover:-translate-y-1 hover:scale-110 mx-auto"
+                                    src=""  alt="">
+                            </div>
+                            <div class="text-center pt-6">
+                                <p class="font-semibold text-3xl">FORHAD HOSSEN</p>
+                                <p class="">Web Developer</p>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="md:w-1/4 sm:w-1/2 w-full px-4">
+                        <div class="group mt-16 ">
+                            <div class="bg-green-50 w-full h-full overflow-hidden">
+                                <img class="bg-bottom transition duration-500 ease-in-out transform hover:-translate-y-1 hover:scale-110 mx-auto"
+                                    src=""  alt="">
+                            </div>
+                            <div class="text-center pt-6">
+                                <p class="font-semibold text-3xl">FORHAD HOSSEN</p>
+                                <p class="">Web Developer</p>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="md:w-1/4 sm:w-1/2 w-full px-4">
+                        <div class="group mt-16 ">
+                            <div class="bg-green-50 w-full h-full overflow-hidden">
+                                <img class="bg-bottom transition duration-500 ease-in-out transform hover:-translate-y-1 hover:scale-110 mx-auto"
+                                    src=""  alt="">
+                            </div>
+                            <div class="text-center pt-6">
+                                <p class="font-semibold text-3xl">FORHAD HOSSEN</p>
+                                <p class="">Web Developer</p>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="md:w-1/4 sm:w-1/2 w-full px-4">
+                        <div class="group mt-16 ">
+                            <div class="bg-green-50 w-full h-full overflow-hidden">
+                                <img class="bg-bottom transition duration-500 ease-in-out transform hover:-translate-y-1 hover:scale-110 mx-auto"
+                                    src=""  alt="">
+                            </div>
+                            <div class="text-center pt-6">
+                                <p class="font-semibold text-3xl">FORHAD HOSSEN</p>
+                                <p class="">Web Developer</p>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="md:w-1/4 sm:w-1/2 w-full px-4">
+                        <div class="group mt-16 ">
+                            <div class="bg-green-50 w-full h-full overflow-hidden">
+                                <img class="bg-bottom transition duration-500 ease-in-out transform hover:-translate-y-1 hover:scale-110 mx-auto"
+                                    src=""  alt="">
+                            </div>
+                            <div class="text-center pt-6">
+                                <p class="font-semibold text-3xl">FORHAD HOSSEN</p>
+                                <p class="">Web Developer</p>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="md:w-1/4 sm:w-1/2 w-full px-4">
+                        <div class="group mt-16 ">
+                            <div class="bg-green-50 w-full h-full overflow-hidden">
+                                <img class="bg-bottom transition duration-500 ease-in-out transform hover:-translate-y-1 hover:scale-110 mx-auto"
+                                    src=""  alt="">
+                            </div>
+                            <div class="text-center pt-6">
+                                <p class="font-semibold text-3xl">FORHAD HOSSEN</p>
+                                <p class="">Web Developer</p>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="md:w-1/4 sm:w-1/2 w-full px-4">
+                        <div class="group mt-16 ">
+                            <div class="bg-green-50 w-full h-full overflow-hidden">
+                                <img class="bg-bottom transition duration-500 ease-in-out transform hover:-translate-y-1 hover:scale-110 mx-auto"
+                                    src=""  alt="">
+                            </div>
+                            <div class="text-center pt-6">
+                                <p class="font-semibold text-3xl">FORHAD HOSSEN</p>
+                                <p class="">Web Developer</p>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="md:w-1/4 sm:w-1/2 w-full px-4">
+                        <div class="group mt-16 ">
+                            <div class="bg-green-50 w-full h-full overflow-hidden">
+                                <img class="bg-bottom transition duration-500 ease-in-out transform hover:-translate-y-1 hover:scale-110 mx-auto"
+                                    src=""  alt="">
+                            </div>
+                            <div class="text-center pt-6">
+                                <p class="font-semibold text-3xl">FORHAD HOSSEN</p>
+                                <p class="">Web Developer</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+</template>
+<script>
+export default {
+    name:'OurTeam'
+}
+</script>
