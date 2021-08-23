223f20bf5ae1999893409e63d46504462862dc60
jamil-codemen <jamil@codemen.net>
2021-08-23 12:01:05 +0600

    Codemen Official
    	1:Navbar Setup

---
+++ src/components/Navigation/Navbar.vue
@@ -0,0 +1,13 @@
+<template>
+<nav>
+    <div id="nav">
+    <router-link to="/">Home</router-link> |
+    <router-link to="/about">About</router-link>
+  </div> 
+</nav>    
+</template>
+<script>
+export default {
+    name:'Navbar'
+}
+</script>
