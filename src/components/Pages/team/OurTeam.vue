7bb2e4eb2288c7a2a592fd9babd275a560110933
Tasnuva Rahman <tasnuva@codemen.net>
2021-08-24 17:00:35 +0600

    tasnuva:creat custom reusable headline component

--- src/components/Pages/team/OurTeam.vue
+++ src/components/Pages/team/OurTeam.vue
@@ -1,25 +1,6 @@
 <template>
-   <div class="bradclumb relative py-32">
-        <div class="bg-center bg-gray-800"></div>
-        <img src="../assets/images/bg-wide2.jpg" alt="image1" class="object-cover w-full h-full absolute top-0 left-0">
-        <div class="container relative z-10 mx-auto md:px-6 px-4 flex flex-wrap items-center justify-center">
-            <div class="slide-content flex sm:text-left justify-center 2xl:w-1/3 xl:w-2/3 sm:w-1/2 w-ful sm:mt-0 mt-32">
-                <div class="">
-                    <nav class="">
-                        <ol class="breadcrumb flex flex-wrap justify-center font-medium text-xl">
-                            <li class="breadcrumb-item text-white"><a href="index.html">Home</a></li>
-                            <span class="px-2 text-white">/</span>
-                            <li class="breadcrumb-item text-green-500  active:text-green-500 " aria-current="page">Our
-                                Team</li>
-                        </ol>
-                    </nav>
-                    <h1 class="md:leading-tight title md:text-7xl text-4xl font-black text-white mt-1.5">
-                        Our Team
-                    </h1>
-                </div>
-            </div>
-        </div>
-    </div>
+
+    <headline imgSource="bg-wide2.jpg" currentPageName="Our Team" currentLink="/ourteam"></headline>
 <section id="our_team" class="md:px-24 px-0 py-24">
         <div class="container max-w-screen-xl mx-auto md:px-6 px-4">
             <div class="">
@@ -200,7 +181,10 @@
     </section>
 </template>
 <script>
+import Headline from '@/components/headline/Headline.vue'
+
 export default {
+  components: { Headline },
     name:'OurTeam'
 }
 </script>
