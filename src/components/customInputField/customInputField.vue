d044f9d9ba01d51b5bab50a51e0dfdb9e232ffe3
jamil-codemen <jamil@codemen.net>
2021-08-25 18:04:43 +0600

    Codemen
    	1:Contact Us page
    	2:there is no icon

---
+++ src/components/customInputField/customInputField.vue
@@ -0,0 +1,25 @@
+<template>
+  <textarea
+    v-if="isTextArea"
+    :name="inputName"
+    :rows="textAreaRows"
+    class="px-4 py-4 w-full focus:outline-none"
+    :class="inputClass"
+    :placeholder="inputPlaceholder"
+  ></textarea>
+
+  <input v-else :class="inputClass" :name="inputName" :placeholder="inputPlaceholder"  :type="inputType" class="px-4 py-4 w-full focus:outline-none" />
+</template>
+<script>
+export default {
+    name:'customInput',
+  props: {
+    isTextArea: Boolean,
+    inputClass: String,
+    inputPlaceholder: String,
+    inputName: String,
+    inputType: String,
+    textAreaRows: String,
+  },
+};
+</script>
