6c106a1aeccfb053e71f9e725d8c07cf62497778
Tasnuva Rahman <tasnuva@codemen.net>
2021-08-25 14:51:38 +0600

    tasnuva creating team page

--- src/components/teamCard/TeamCard.vue
+++ src/components/teamCard/TeamCard.vue
@@ -2,10 +2,10 @@
         <div class="group">
             <div class="bg-green-50 w-full h-full overflow-hidden">
                 <img class="bg-bottom transition duration-500 ease-in-out transform hover:-translate-y-1 hover:scale-110 mx-auto"
-                    :src="require(`@/assets/images/${imgSource}`)" alt="">
+                    :src="require(`@/assets/images/team/${imgSource}`)" alt="">
             </div>
             <div class="text-center pt-6">
-                <p class="font-semibold text-3xl">{{ employeeName }}</p>
+                <p class="font-semibold text-2xl">{{ employeeName }}</p>
                 <p class="">{{ employeeRole }}</p>
             </div>
         </div>
