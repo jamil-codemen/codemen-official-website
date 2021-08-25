f703a0176f87b98a787c39392bf10c4923eb4f8f
Tasnuva Rahman <tasnuva@codemen.net>
2021-08-25 14:51:38 +0600

    tasnuva creating team page

--- src/Pages/team/OurTeam.vue
+++ src/Pages/team/OurTeam.vue
@@ -1,189 +1,96 @@
 <template>
     <headline imgSource="bg-wide2.jpg" currentPageName="Our Team" currentLink="/ourteam"></headline>
-<section id="our_team" class="md:px-24 px-0 py-24">
-        <div class="container max-w-screen-xl mx-auto md:px-6 px-4">
+<section id="our_team" class="md:px-24 px-0 pt-24 ">
+        <div class="container max-w-screen-xl mx-auto md:px-6 px-4 py-12">
             <div class="">
-                <div class="flex flex-wrap justify-center">
-                    <div class="md:w-1/4 sm:w-1/2 w-full px-4">
-                        <div class="group md:mt-0 mt-10 ">
-                            <div class="bg-green-50 w-full h-full overflow-hidden">
-                                <img class="bg-bottom transition duration-500 ease-in-out transform hover:-translate-y-1 hover:scale-110 mx-auto"
-                                    src=""  alt="">
-                            </div>
-                            <div class="text-center pt-6">
-                                <p class="font-semibold text-3xl">FORHAD HOSSEN</p>
-                                <p class="">Web Developer</p>
-                            </div>
-                        </div>
+                <div class="text-center mb-12"><span class="prm-color rounded text-white px-4 pb-4 pt-2 font-bold text-4xl ">Founders</span></div>
+                <div class="row justify-center">
+                    <div class="md:w-1/4 sm:w-1/2 w-full md:pt-0 pt-10 px-4 ">
+                       <team-card imgSource="full_imagemen.png" employeeName="Forhad Hosen" employeeRole="Web Developer"></team-card>
                     </div>
-                    <div class="md:w-1/4 sm:w-1/2 w-full px-4">
-                        <div class="group md:mt-0 mt-10 ">
-                            <div class="bg-green-50 w-full h-full overflow-hidden">
-                                <img class="bg-bottom transition duration-500 ease-in-out transform hover:-translate-y-1 hover:scale-110 mx-auto"
-                                    src=""  alt="">
-                            </div>
-                            <div class="text-center pt-6">
-                                <p class="font-semibold text-3xl">FORHAD HOSSEN</p>
-                                <p class="">Web Developer</p>
-                            </div>
-                        </div>
+                     <div class="md:w-1/4 sm:w-1/2 w-full md:pt-0 pt-10 px-4">
+                       <team-card imgSource="full_imagemen.png" employeeName="Forhad Hosen" employeeRole="Web Developer"></team-card>
                     </div>
-                    <div class="md:w-1/4 sm:w-1/2 w-full px-4">
-                        <div class="group md:mt-0 mt-10 ">
-                            <div class="bg-green-50 w-full h-full overflow-hidden">
-                                <img class="bg-bottom transition duration-500 ease-in-out transform hover:-translate-y-1 hover:scale-110 mx-a"
-                                    src=""  alt="">
-                            </div>
-                            <div class="text-center pt-6">
-                                <p class="font-semibold text-3xl">FORHAD HOSSEN</p>
-                                <p class="">Web Developer</p>
-                            </div>
-                        </div>
+                    <div class="md:w-1/4 sm:w-1/2 w-full md:pt-0 pt-10 px-4">
+                       <team-card imgSource="full_imagemen.png" employeeName="Forhad Hosen" employeeRole="Web Developer"></team-card>
                     </div>
                 </div>
-                <div class="flex flex-wrap justify-center">
-                    <div class="md:w-1/4 sm:w-1/2 w-full px-4">
-                        <div class="group mt-16 ">
-                            <div class="bg-green-50 w-full h-full overflow-hidden">
-                                <img class="bg-bottom transition duration-500 ease-in-out transform hover:-translate-y-1 hover:scale-110 mx-auto"
-                                    src=""  alt="">
-                            </div>
-                            <div class="text-center pt-6">
-                                <p class="font-semibold text-3xl">FORHAD HOSSEN</p>
-                                <p class="">Web Developer</p>
-                            </div>
-                        </div>
+                <div class="text-center mt-24 mb-12"><span class="prm-color rounded text-white px-4 pb-4 pt-2 font-bold text-4xl ">Full Stack</span></div>
+                <div class="row justify-center">
+                    <div class="md:w-1/4 sm:w-1/2 w-full md:pt-0 pt-10 px-4 ">
+                       <team-card imgSource="full_imagemen.png" employeeName="Forhad Hosen" employeeRole="Web Developer"></team-card>
                     </div>
-                    <div class="md:w-1/4 sm:w-1/2 w-full px-4">
-                        <div class="group mt-16 ">
-                            <div class="bg-green-50 w-full h-full overflow-hidden">
-                                <img class="bg-bottom transition duration-500 ease-in-out transform hover:-translate-y-1 hover:scale-110 mx-auto"
-                                    src=""  alt="">
-                            </div>
-                            <div class="text-center pt-6">
-                                <p class="font-semibold text-3xl">FORHAD HOSSEN</p>
-                                <p class="">Web Developer</p>
-                            </div>
-                        </div>
+                     <div class="md:w-1/4 sm:w-1/2 w-full md:pt-0 pt-10 px-4">
+                       <team-card imgSource="full_imagemen.png" employeeName="Forhad Hosen" employeeRole="Web Developer"></team-card>
                     </div>
-                    <div class="md:w-1/4 sm:w-1/2 w-full px-4">
-                        <div class="group mt-16 ">
-                            <div class="bg-green-50 w-full h-full overflow-hidden">
-                                <img class="bg-bottom transition duration-500 ease-in-out transform hover:-translate-y-1 hover:scale-110 mx-auto"
-                                    src=""  alt="">
-                            </div>
-                            <div class="text-center pt-6">
-                                <p class="font-semibold text-3xl">FORHAD HOSSEN</p>
-                                <p class="">Web Developer</p>
-                            </div>
-                        </div>
+                </div>
+                <div class="text-center mt-24 mb-12"><span class="prm-color rounded text-white px-4 pb-4 pt-2 font-bold text-4xl ">Front End</span></div>
+                <div class="row justify-center">
+                    <div class="md:w-1/4 sm:w-1/2 w-full md:pt-0 pt-10 px-4 ">
+                       <team-card imgSource="full_imagemen.png" employeeName="Forhad Hosen" employeeRole="Web Developer"></team-card>
                     </div>
-                    <div class="md:w-1/4 sm:w-1/2 w-full px-4">
-                        <div class="group mt-16 ">
-                            <div class="bg-green-50 w-full h-full overflow-hidden">
-                                <img class="bg-bottom transition duration-500 ease-in-out transform hover:-translate-y-1 hover:scale-110 mx-auto"
-                                    src=""  alt="">
-                            </div>
-                            <div class="text-center pt-6">
-                                <p class="font-semibold text-3xl">FORHAD HOSSEN</p>
-                                <p class="">Web Developer</p>
-                            </div>
-                        </div>
+                     <div class="md:w-1/4 sm:w-1/2 w-full md:pt-0 pt-10 px-4">
+                       <team-card imgSource="full_imagemen.png" employeeName="Forhad Hosen" employeeRole="Web Developer"></team-card>
                     </div>
-                    <div class="md:w-1/4 sm:w-1/2 w-full px-4">
-                        <div class="group mt-16 ">
-                            <div class="bg-green-50 w-full h-full overflow-hidden">
-                                <img class="bg-bottom transition duration-500 ease-in-out transform hover:-translate-y-1 hover:scale-110 mx-auto"
-                                    src=""  alt="">
-                            </div>
-                            <div class="text-center pt-6">
-                                <p class="font-semibold text-3xl">FORHAD HOSSEN</p>
-                                <p class="">Web Developer</p>
-                            </div>
-                        </div>
+                    <div class="md:w-1/4 sm:w-1/2 w-full md:pt-0 pt-10 px-4 ">
+                       <team-card imgSource="full_imagemen.png" employeeName="Forhad Hosen" employeeRole="Web Developer"></team-card>
                     </div>
-                    <div class="md:w-1/4 sm:w-1/2 w-full px-4">
-                        <div class="group mt-16 ">
-                            <div class="bg-green-50 w-full h-full overflow-hidden">
-                                <img class="bg-bottom transition duration-500 ease-in-out transform hover:-translate-y-1 hover:scale-110 mx-auto"
-                                    src=""  alt="">
-                            </div>
-                            <div class="text-center pt-6">
-                                <p class="font-semibold text-3xl">FORHAD HOSSEN</p>
-                                <p class="">Web Developer</p>
-                            </div>
-                        </div>
+                     <div class="md:w-1/4 sm:w-1/2 w-full md:pt-0 pt-10 px-4">
+                       <team-card imgSource="full_imagemen.png" employeeName="Forhad Hosen" employeeRole="Web Developer"></team-card>
                     </div>
-                    <div class="md:w-1/4 sm:w-1/2 w-full px-4">
-                        <div class="group mt-16 ">
-                            <div class="bg-green-50 w-full h-full overflow-hidden">
-                                <img class="bg-bottom transition duration-500 ease-in-out transform hover:-translate-y-1 hover:scale-110 mx-auto"
-                                    src=""  alt="">
-                            </div>
-                            <div class="text-center pt-6">
-                                <p class="font-semibold text-3xl">FORHAD HOSSEN</p>
-                                <p class="">Web Developer</p>
-                            </div>
-                        </div>
+                    <div class="md:w-1/4 sm:w-1/2 w-full md:pt-0 pt-10 px-4 ">
+                       <team-card imgSource="full_imagemen.png" employeeName="Forhad Hosen" employeeRole="Web Developer"></team-card>
                     </div>
-                    <div class="md:w-1/4 sm:w-1/2 w-full px-4">
-                        <div class="group mt-16 ">
-                            <div class="bg-green-50 w-full h-full overflow-hidden">
-                                <img class="bg-bottom transition duration-500 ease-in-out transform hover:-translate-y-1 hover:scale-110 mx-auto"
-                                    src=""  alt="">
-                            </div>
-                            <div class="text-center pt-6">
-                                <p class="font-semibold text-3xl">FORHAD HOSSEN</p>
-                                <p class="">Web Developer</p>
-                            </div>
-                        </div>
+                     <div class="md:w-1/4 sm:w-1/2 w-full md:pt-0 pt-10 px-4">
+                       <team-card imgSource="full_imagemen.png" employeeName="Forhad Hosen" employeeRole="Web Developer"></team-card>
                     </div>
-                    <div class="md:w-1/4 sm:w-1/2 w-full px-4">
-                        <div class="group mt-16 ">
-                            <div class="bg-green-50 w-full h-full overflow-hidden">
-                                <img class="bg-bottom transition duration-500 ease-in-out transform hover:-translate-y-1 hover:scale-110 mx-auto"
-                                    src=""  alt="">
-                            </div>
-                            <div class="text-center pt-6">
-                                <p class="font-semibold text-3xl">FORHAD HOSSEN</p>
-                                <p class="">Web Developer</p>
-                            </div>
-                        </div>
+                </div>
+                 <div class="text-center mt-24 mb-12"><span class="prm-color rounded text-white px-4 pb-4 pt-2 font-bold text-4xl ">Back End</span></div>
+                <div class="row justify-center">
+                    <div class="md:w-1/4 sm:w-1/2 w-full md:pt-0 pt-10 px-4 ">
+                       <team-card imgSource="full_imagemen.png" employeeName="Forhad Hosen" employeeRole="Web Developer"></team-card>
                     </div>
-                    <div class="md:w-1/4 sm:w-1/2 w-full px-4">
-                        <div class="group mt-16 ">
-                            <div class="bg-green-50 w-full h-full overflow-hidden">
-                                <img class="bg-bottom transition duration-500 ease-in-out transform hover:-translate-y-1 hover:scale-110 mx-auto"
-                                    src=""  alt="">
-                            </div>
-                            <div class="text-center pt-6">
-                                <p class="font-semibold text-3xl">FORHAD HOSSEN</p>
-                                <p class="">Web Developer</p>
-                            </div>
-                        </div>
+                     <div class="md:w-1/4 sm:w-1/2 w-full md:pt-0 pt-10 px-4">
+                       <team-card imgSource="full_imagemen.png" employeeName="Forhad Hosen" employeeRole="Web Developer"></team-card>
                     </div>
-                    <div class="md:w-1/4 sm:w-1/2 w-full px-4">
-                        <div class="group mt-16 ">
-                            <div class="bg-green-50 w-full h-full overflow-hidden">
-                                <img class="bg-bottom transition duration-500 ease-in-out transform hover:-translate-y-1 hover:scale-110 mx-auto"
-                                    src=""  alt="">
-                            </div>
-                            <div class="text-center pt-6">
-                                <p class="font-semibold text-3xl">FORHAD HOSSEN</p>
-                                <p class="">Web Developer</p>
-                            </div>
-                        </div>
+                    <div class="md:w-1/4 sm:w-1/2 w-full md:pt-0 pt-10 px-4 ">
+                       <team-card imgSource="full_imagemen.png" employeeName="Forhad Hosen" employeeRole="Web Developer"></team-card>
+                    </div>
+                     <div class="md:w-1/4 sm:w-1/2 w-full md:pt-0 pt-10 px-4">
+                       <team-card imgSource="full_imagemen.png" employeeName="Forhad Hosen" employeeRole="Web Developer"></team-card>
+                    </div>
+                    <div class="md:w-1/4 sm:w-1/2 w-full md:pt-0 pt-10 px-4 ">
+                       <team-card imgSource="full_imagemen.png" employeeName="Forhad Hosen" employeeRole="Web Developer"></team-card>
+                    </div>
+                     <div class="md:w-1/4 sm:w-1/2 w-full md:pt-0 pt-10 px-4">
+                       <team-card imgSource="full_imagemen.png" employeeName="Forhad Hosen" employeeRole="Web Developer"></team-card>
+                    </div>
+                    <div class="md:w-1/4 sm:w-1/2 w-full md:pt-0 pt-10 px-4 ">
+                       <team-card imgSource="full_imagemen.png" employeeName="Forhad Hosen" employeeRole="Web Developer"></team-card>
+                    </div>
+                     <div class="md:w-1/4 sm:w-1/2 w-full md:pt-0 pt-10 px-4">
+                       <team-card imgSource="full_imagemen.png" employeeName="Forhad Hosen" employeeRole="Web Developer"></team-card>
                     </div>
                 </div>
             </div>
+        </div>
+        <div class="md:px-24 px-0 pb-24 mt-16">
+            <promotion-card></promotion-card>
         </div>
     </section>
 </template>
 <script>
 import Headline from '@/components/headline/Headline.vue'
+import TeamCard from '@/components/teamCard/TeamCard.vue'
+import PromotionCard from '@/components/promotionCard/PromotionCard.vue'
 
 export default {
-  components: { Headline },
+  components: { Headline, TeamCard, PromotionCard },
     name:'OurTeam'
 }
 </script>
+<style scoped>
+.prm-color{
+    background-color:#113335;
+}
+</style>
---
+++ src/components/promotionCard/PromotionCard.vue
@@ -0,0 +1,18 @@
+<template>
+    <div class="container max-w-screen-xl mx-auto md:px-6 bg-green-200 py-14 my-4 rounded">
+            <div class="text-center">
+                <h2 class="py-6 font-bold text-4xl">Codemen unlocks your financial potential</h2>
+                <p class="py-2 text-gray-700 text-xl">Through our platform, you can access a network of APIs, banks and other financial<br> institutions easily. Ready to start your journey with us?</p>
+                <div class="mt-4 sm:mx-0 mx-4">
+                    <button class="hover:bg-gray-900 py-4 pl-8 pr-2 rounded justify-center  sm:w-52 w-full bg-green-500 font-bold text-white items-center">
+                        <a class="mr-6 md:text-lg leading-8" href="javascript:;">Get Started</a>                            
+                    </button>
+                </div>  
+            </div>
+        </div>
+</template>
+<script lang="ts">
+export default {
+    name:"PromotionCard"
+}
+</script>
