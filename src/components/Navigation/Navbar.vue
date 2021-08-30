7e8f3906da2b30268d839b366de8c099b573e153
Tasnuva Rahman <tasnuva@codemen.net>
2021-08-30 17:53:48 +0600

    tasnuva:fixing navbar

--- src/components/Navigation/Navbar.vue
+++ src/components/Navigation/Navbar.vue
@@ -6,19 +6,8 @@
         <div class="row">
           <div class="md:w-1/2 px-4 w-full">
             <div class="flex items-center md:justify-start justify-center">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                class="h-4 w-4 text-gray-300"
-                fill="none"
-                viewBox="0 0 24 24"
-                stroke="currentColor"
-              >
-                <path
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
-                  stroke-width="2"
-                  d="M12 8v4l3 3m6-3a9 9 0 11-18 0 9 9 0 0118 0z"
-                />
+              <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5 text-gray-300" viewBox="0 0 20 20" fill="currentColor">
+                <path fill-rule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm1-12a1 1 0 10-2 0v4a1 1 0 00.293.707l2.828 2.829a1 1 0 101.415-1.415L11 9.586V6z" clip-rule="evenodd" />
               </svg>
               <span class="text font-normal text-base text-gray-400 px-2">
                 Open Hours: Mon - Fri - 10:00 AM - 07:00 PM
@@ -27,47 +16,91 @@
           </div>
           <div class="md:w-1/2 px-4 w-full">
             <div
-              class="flex flex-wrap md:justify-end justify-center items-center space-x-2"
+              class="
+                flex
+                md:justify-end
+                justify-center
+                items-center
+                space-x-5
+              "
             >
               <div class="flex items-center">
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  class="h-4 w-4 text-gray-300"
-                  fill="none"
-                  viewBox="0 0 24 24"
-                  stroke="currentColor"
-                >
-                  <path
-                    stroke-linecap="round"
-                    stroke-linejoin="round"
-                    stroke-width="2"
-                    d="M3 8l7.89 5.26a2 2 0 002.22 0L21 8M5 19h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v10a2 2 0 002 2z"
-                  />
+                <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5 text-gray-300" viewBox="0 0 20 20" fill="currentColor">
+                  <path d="M2.003 5.884L10 9.882l7.997-3.998A2 2 0 0016 4H4a2 2 0 00-1.997 1.884z" />
+                  <path d="M18 8.118l-8 4-8-4V14a2 2 0 002 2h12a2 2 0 002-2V8.118z" />
                 </svg>
                 <span class="text font-normal text-base text-gray-400 px-2">
                   codemenorg@gmail.com
                 </span>
               </div>
               <a href="javascript:;" class="">
-                <img
-                  class="h-4 w-4 "
-                  src="../../assets/icons/facebook.svg"
-                  alt=""
-                />
+                <svg
+                  version="1.1"
+                  id="Capa_1"
+                  xmlns="http://www.w3.org/2000/svg"
+                  xmlns:xlink="http://www.w3.org/1999/xlink"
+                  x="0px"
+                  y="0px"
+                  class="h-4 w-4 text-gray-300 hover:text-green-400"
+                  fill="currentColor"
+                  viewBox="0 0 512 512"
+                  style="enable-background: new 0 0 512 512"
+                  xml:space="preserve"
+                >
+                  <g>
+                    <g>
+                      <path
+                        d="M288,176v-64c0-17.664,14.336-32,32-32h32V0h-64c-53.024,0-96,42.976-96,96v80h-64v80h64v256h96V256h64l32-80H288z"
+                      />
+                    </g>
+                  </g>
+                </svg>
               </a>
               <a href="javascript:;" class="">
-                <img
-                  class="h-4 w-4 "
-                  src="../../assets/icons/twitter.svg"
-                  alt=""
-                />
+                <svg
+                  version="1.1"
+                  id="Capa_1"
+                  xmlns="http://www.w3.org/2000/svg"
+                  xmlns:xlink="http://www.w3.org/1999/xlink"
+                  x="0px"
+                  y="0px"
+                  class="h-4 w-4 text-gray-300 hover:text-green-400"
+                  fill="currentColor"
+                  viewBox="0 0 512 512"
+                  style="enable-background: new 0 0 512 512"
+                  xml:space="preserve"
+                >
+                  <g>
+                    <path
+                      d="M512,97.248c-19.04,8.352-39.328,13.888-60.48,16.576c21.76-12.992,38.368-33.408,46.176-58.016
+                    c-20.288,12.096-42.688,20.64-66.56,25.408C411.872,60.704,384.416,48,354.464,48c-58.112,0-104.896,47.168-104.896,104.992
+                    c0,8.32,0.704,16.32,2.432,23.936c-87.264-4.256-164.48-46.08-216.352-109.792c-9.056,15.712-14.368,33.696-14.368,53.056
+                    c0,36.352,18.72,68.576,46.624,87.232c-16.864-0.32-33.408-5.216-47.424-12.928c0,0.32,0,0.736,0,1.152
+                    c0,51.008,36.384,93.376,84.096,103.136c-8.544,2.336-17.856,3.456-27.52,3.456c-6.72,0-13.504-0.384-19.872-1.792
+                    c13.6,41.568,52.192,72.128,98.08,73.12c-35.712,27.936-81.056,44.768-130.144,44.768c-8.608,0-16.864-0.384-25.12-1.44
+                    C46.496,446.88,101.6,464,161.024,464c193.152,0,298.752-160,298.752-298.688c0-4.64-0.16-9.12-0.384-13.568
+                    C480.224,136.96,497.728,118.496,512,97.248z"
+                    />
+                  </g>
+                </svg>
               </a>
               <a href="javascript:;" class="">
-                <img
-                  class="h-4 w-4 "
-                  src="../../assets/icons/linkedin.svg"
-                  alt=""
-                />
+                <svg
+                  enable-background="new 0 0 24 24"
+                  height="512"
+                  viewBox="0 0 24 24"
+                  class="h-4 w-4 text-gray-300 hover:text-green-400"
+                  fill="currentColor"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <path
+                    d="m23.994 24v-.001h.006v-8.802c0-4.306-.927-7.623-5.961-7.623-2.42 0-4.044 1.328-4.707 2.587h-.07v-2.185h-4.773v16.023h4.97v-7.934c0-2.089.396-4.109 2.983-4.109 2.549 0 2.587 2.384 2.587 4.243v7.801z"
+                  />
+                  <path d="m.396 7.977h4.976v16.023h-4.976z" />
+                  <path
+                    d="m2.882 0c-1.591 0-2.882 1.291-2.882 2.882s1.291 2.909 2.882 2.909 2.882-1.318 2.882-2.909c-.001-1.591-1.292-2.882-2.882-2.882z"
+                  />
+                </svg>
               </a>
             </div>
           </div>
@@ -85,29 +118,32 @@
             </div>
             <!-- Wide Screen Nav -->
             <div class="w-2/3 px-4">
-              <ul class="flex space-x-14 justify-end uppercase font-bold text-base text-white">
+              <ul
+                class="
+                  flex
+                  space-x-14
+                  justify-end
+                  uppercase
+                  font-bold
+                  text-base text-white
+                "
+              >
                 <li class="hover:text-green-500">
                   <router-link to="/" active-class="text-green-500"
                     >Home</router-link
                   >
                 </li>
-                <li
-                  class="hover:text-green-500"
-                >
+                <li class="hover:text-green-500">
                   <router-link to="/about" active-class="text-green-500"
                     >About Us</router-link
                   >
                 </li>
-                <li
-                  class="hover:text-green-500"
-                >
+                <li class="hover:text-green-500">
                   <router-link to="/ourteam" active-class="text-green-500"
                     >Our Team</router-link
                   >
                 </li>
-                <li
-                  class="hover:text-green-500"
-                >
+                <li class="hover:text-green-500">
                   <router-link to="/contact-us" active-class="text-green-500"
                     >Contact</router-link
                   >
@@ -122,46 +158,89 @@
         <div class="flex items-center justify-between mx-4 py-4">
           <img src="../../assets/images/codemen-logo2.png" alt="" />
           <div @click="toggle" class="ml-auto block md:hidden">
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            class="h-6 w-6 text-white"
-            fill="none"
-            viewBox="0 0 24 24"
-            stroke="currentColor"
-          >
-            <path
-              stroke-linecap="round"
-              stroke-linejoin="round"
-              stroke-width="2"
-              d="M4 6h16M4 12h16M4 18h16"
-            />
-          </svg>
-        </div>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              class="h-6 w-6 text-white"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+            >
+              <path
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                stroke-width="2"
+                d="M4 6h16M4 12h16M4 18h16"
+              />
+            </svg>
+          </div>
         </div>
         <!-- Mobile Nav -->
-        <div :class="open ? 'hidden' : 'block'" class="bg-cst-color mx-4 block md:hidden">
+        <div
+          :class="open ? 'hidden' : 'block'"
+          class="bg-cst-color mx-4 block md:hidden"
+        >
           <div class="">
             <div class="">
               <ul class="space-y-1">
                 <li
-                  class="uppercase font-bold text-lg text-white hover:text-green-600 hover:bg-gray-900 active:text-green-500"
+                  class="
+                    uppercase
+                    font-bold
+                    text-lg text-white
+                    hover:text-green-600
+                    hover:bg-gray-900
+                    active:text-green-500
+                    text-center
+                  "
                 >
-                  <router-link to="/" class="inline-block ml-3 py-3">Home</router-link>
+                  <router-link to="/" class="inline-block ml-3 py-3"
+                    >Home</router-link
+                  >
                 </li>
                 <li
-                  class="uppercase font-bold text-lg text-white hover:text-green-600 hover:bg-gray-900 active:text-green-500"
+                  class="
+                    uppercase
+                    font-bold
+                    text-lg text-white
+                    hover:text-green-600
+                    hover:bg-gray-900
+                    active:text-green-500
+                    text-center
+                  "
                 >
-                  <router-link to="/about" class="inline-block ml-3 py-3">About Us</router-link>
+                  <router-link to="/about" class="inline-block ml-3 py-3"
+                    >About Us</router-link
+                  >
                 </li>
                 <li
-                  class="uppercase font-bold text-lg text-white hover:text-green-600 hover:bg-gray-900 active:text-green-500"
+                  class="
+                    uppercase
+                    font-bold
+                    text-lg text-white
+                    hover:text-green-600
+                    hover:bg-gray-900
+                    active:text-green-500
+                    text-center
+                  "
                 >
-                  <router-link to="/ourteam" class="inline-block ml-3 py-3">Our Team</router-link>
+                  <router-link to="/ourteam" class="inline-block ml-3 py-3"
+                    >Our Team</router-link
+                  >
                 </li>
                 <li
-                  class="uppercase font-bold text-lg text-white hover:text-green-600 hover:bg-gray-900 active:text-green-500"
+                  class="
+                    uppercase
+                    font-bold
+                    text-lg text-white
+                    hover:text-green-600
+                    hover:bg-gray-900
+                    active:text-green-500
+                    text-center
+                  "
                 >
-                  <router-link to="/contact-us" class="inline-block ml-3 py-3">Contact</router-link>
+                  <router-link to="/contact-us" class="inline-block ml-3 py-3"
+                    >Contact</router-link
+                  >
                 </li>
               </ul>
             </div>
