0fd61464c395daf7d23082f1b15692b1fa118ad8
Tasnuva Rahman <tasnuva@codemen.net>
2021-09-01 16:20:22 +0600

    tasnuva:added form validation for all forms (contact ,login,forgot password)

--- src/components/customInputField/customInputField.vue
+++ src/components/customInputField/customInputField.vue
@@ -7,9 +7,14 @@
     class="px-4 py-4 w-full focus:outline-none"
     :class="inputClass"
     :placeholder="inputPlaceholder"
+    :value="modelValue" 
+    @input="$emit('update:modelValue', $event.target.value)" 
+    @blur="$emit('blur',$event.target.value)" 
+    @focus="$emit('focus',$event.target.value)"
+    required
   ></textarea>
 <!------------------------------------------    input component ------------------------------------------------->
-  <input v-else :class="inputClass" :name="inputName" :placeholder="inputPlaceholder"  :type="inputType" class="px-4 py-4 w-full focus:outline-none" />
+  <input v-else :class="inputClass" :name="inputName" :placeholder="inputPlaceholder"  :type="inputType" class="px-4 py-4 w-full focus:outline-none" :value="modelValue" @input="$emit('update:modelValue', $event.target.value)" @blur="$emit('blur',$event.target.value)" @focus="$emit('focus',$event.target.value)" required/>
 </template>
 <script>
 export default {
@@ -21,6 +26,8 @@ export default {
     inputName: String,
     inputType: String,
     textAreaRows: String,
+    modelValue:String
   },
+  emits: ['update:modelValue','blur','focus'],
 };
 </script>
